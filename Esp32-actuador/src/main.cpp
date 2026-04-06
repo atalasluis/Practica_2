b84733175ff9d26479c54fc53f973328b991b31c
Juanma <juanma.soruco@gmail.com>
2026-04-06 15:42:57 -0400

    feat: controler and sensor code

--- Esp32-actuador/src/main.cpp
+++ Esp32-actuador/src/main.cpp
@@ -1,18 +1,74 @@
-#include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "TU_WIFI";
+const char* password = "TU_PASSWORD";
+
+const char* host = "192.168.1.100"; 
+const int port = 5000;
+
+#define LED_RED 5
+#define LED_YELLOW 18
+#define LED_GREEN 19
+
+WiFiClient client;
+
+void apagarTodos() {
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, LOW);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi conectado");
+
+  while (!client.connect(host, port)) {
+    Serial.println("Conectando al servidor...");
+    delay(2000);
+  }
+
+  Serial.println("Conectado al servidor");
+
+  client.println("ACTUADOR");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (!client.connected()) {
+    Serial.println("Reconectando...");
+    client.connect(host, port);
+    client.println("ACTUADOR");
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (client.available()) {
+    String comando = client.readStringUntil('\n');
+    comando.trim();
+
+    Serial.print("Comando recibido: ");
+    Serial.println(comando);
+
+    apagarTodos();
+
+    if (comando == "RED") {
+      digitalWrite(LED_RED, HIGH);
+    }
+    else if (comando == "YELLOW") {
+      digitalWrite(LED_YELLOW, HIGH);
+    }
+    else if (comando == "GREEN") {
+      digitalWrite(LED_GREEN, HIGH);
+    }
+  }
 }
--- Esp32-sensor/src/main.cpp
+++ Esp32-sensor/src/main.cpp
@@ -1,18 +1,74 @@
-#include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "TU_WIFI";
+const char* password = "TU_PASSWORD";
+
+const char* host = "192.168.1.100"; 
+const int port = 5000;
+
+#define TRIG 5
+#define ECHO 18
+
+WiFiClient client;
+
+long medirDistancia() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+
+  long duracion = pulseIn(ECHO, HIGH);
+  long distancia = duracion * 0.034 / 2;
+
+  return distancia;
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+  // WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi conectado");
+
+  // Conectar al servidor
+  while (!client.connect(host, port)) {
+    Serial.println("Conectando al servidor...");
+    delay(2000);
+  }
+
+  Serial.println("Conectado al servidor");
+
+  // Registrarse como SENSOR
+  client.println("SENSOR");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (!client.connected()) {
+    Serial.println("Reconectando...");
+    client.connect(host, port);
+    client.println("SENSOR");
+  }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  long distancia = medirDistancia();
+
+  Serial.print("Distancia: ");
+  Serial.println(distancia);
+
+  // Enviar al servidor
+  client.print("DIST:");
+  client.println(distancia);
+
+  delay(2000); 
 }
